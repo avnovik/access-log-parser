e4cb9207e609564049ebf046a8faf00cb3cd82bc
AVNovikov <alekseivnovikov@vtb.ru>
2024-07-18 20:26:30 +0300

    added hw3.2

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Введите текст и нажмите <Enter>:");
+        System.out.println("Введите первое число:");
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
 
-        String text = new Scanner(System.in).nextLine();
+        System.out.println("Введите второе число:");
+        int number2 = scanner.nextInt();
 
-        System.out.println("Длинна текста: " + text.length());
+        scanner.close();
+
+        System.out.println("Сумма: " + (number1 + number2));
+        System.out.println("Разность: " + (number1 - number2));
+        System.out.println("Произведение: " + (number1 * number2));
+        System.out.println("Частное: " + (double)(number1 / number2));
     }
 }
