106f483ec9efeafdae4e4109050fd37da2ff3f27
AVNovikov <alekseivnovikov@vtb.ru>
2024-08-19 22:24:47 +0300

    added file.exists fun

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,33 @@
+import java.io.File;
 import java.util.Scanner;
+
+/**
+ * C:/Users/Desktop/test/1.txt
+ */
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Введите текст и нажмите <Enter>:");
+        var counts = 0;
 
-        String text = new Scanner(System.in).nextLine();
+        while (true) {
+            System.out.print("Введите путь: ");
+            String path = new Scanner(System.in).nextLine();
 
-        System.out.println("Длинна текста: " + text.length());
+            File file = new File(path);
+
+            boolean fileExists = file.exists();
+
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists || isDirectory) {
+                System.out.println("Путь указан НЕ верно");
+                continue;
+            } else {
+                System.out.println("Путь указан верно");
+                counts++;
+                System.out.println("Это файл номер " + counts);
+            }
+        }
     }
 }
